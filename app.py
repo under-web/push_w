9d40707833c4ff181e983745dd614c758b970086
Artem <furens@protonmail.com>
2021-07-30 15:34:23 +0300

    add city

--- app.py
+++ app.py
@@ -5,35 +5,38 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
-def get_html(url):
+def get_html(url):  # принимает урл подменяет юзер-агент возвращает html-текст
     ua = UserAgent()
     headers = {'User-Agent': ua.firefox}
     res = requests.get(url, headers=headers)
     return res.text
 
 
-def get_weather(res):
+def get_weather(res):  # парсит погоду
     soup = BeautifulSoup(res, 'lxml')
+    block_geo = soup.find('h1').text
     block_w = soup.find('div', class_='tab_wrap')
     block_har = soup.find('div', class_='tab tooltip').get('data-text')
     time_w = block_w.find('div', class_="date xs fadeIn").text.strip()
     temp_w = block_w.find('span', class_="unit unit_temperature_c").text.strip()
-    result_note = time_w + '\t' + temp_w + '\t' + block_har
+    result_note = block_geo.strip() + '\n' + time_w + '\t' + temp_w + '\t' + block_har
     return result_note
 
 
-def show_note(result_note):
+def show_note(result_note):  # выводит сообщение
     plyer.notification.notify(message=result_note,
                               app_name='wApp',
                               app_icon='1.ico',
                               title='Погода')
 
 
-def main():
+def main():  # главная функция
     while True:
         try:
-            url = 'https://www.gismeteo.ru/weather-apastovo-204190/'
-            show_note(get_weather(get_html(url)))
+            urls = ['https://www.gismeteo.ru/weather-apastovo-204190/',
+                   'https://www.gismeteo.ru/weather-bugulma-4583/']
+            for url in urls:
+                show_note(get_weather(get_html(url)))
             time.sleep(3600)
         except Exception as e:
             time.sleep(10)
